e66bed7208b882ce30e9d5ce088b88fb184771f4
chatherine <akoakatsiri@gmail.com>
2016-06-26 02:04:00 +0000

    Initial commit

--- app.rb
+++ app.rb
@@ -1,3 +1,45 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+get '/' do
+    erb :form
+end
+
+get '/list' do
+    uri = URI("http://express.heartrails.com/api/json")
+    uri.query = URI.encode_www_form({
+        method: "getStations",
+        x: params[:x],
+        y: params[:y]
+    })
+    res = Net::HTTP.get_response(uri)
+    json = JSON.parse(res.body)
+    @stations = json["response"]["station"]
+    erb :list
+end
+
+get '/api/station' do
+    uri = URI("http://express.heartrails.com/api/json")
+    uri.query = URI.encode_www_form({
+        method: "getStations",
+        line: params[:line],
+        name: params[:name]
+    })
+    res = Net::HTTP.get_response(uri)
+    @resurl = res
+    json = JSON.parse(res.body)
+    if json["response"]["error"]
+        response = {error: "No Station."}
+    else
+        response = {
+            next: json["response"]["station"][0]["next"],
+            prev: json["response"]["station"][0]["prev"]
+        }
+    end
+    
+    json response
+end
